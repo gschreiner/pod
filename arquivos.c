d23c835ecc7eafd9d2036531012d8e0c0d1c530f
gschreiner <geomarschreiner@gmail.com>
2020-11-04 17:57:15 -0300

    IO basico em C

---
+++ arquivos.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+
+    FILE *file;
+    int i,a [5] = {0,1,2,3,434};
+    char string[13];
+
+    file = fopen("nomeDoArquivo.aul", "r+");
+    if (file == NULL){
+        printf("Deu caca!\n");
+        return 1;
+    }
+
+    for (i=0; i< 5; i++){
+        fprintf(file, "%d\n", a[i]);
+    }
+    fclose(file);
+
+    file = fopen("nomeDoArquivo.aul", "r");
+    if (file == NULL){
+        printf("Deu caca!\n");
+        return 1;
+    }
+
+    for (i=0; i< 5; i++){
+        fscanf(file, "%d\n", &a[i]);
+        printf("lemos: %d\n", a[i]);
+    }
+
+   //fget -> char by char
+   //fgets -> linha, ou o tamanho que passarmos como parametro.
+   //fscanf - > lê qualquer coisa.
+
+    fclose(file);
+
+
+    return 0;
+}
